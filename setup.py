2312ee773fa303874d6fcc46f8f26444cea272fa
trytodupe <liuandi@live.com>
2025-07-06 22:02:16 +0800

    chore: dependencies in setup.py

--- setup.py
+++ setup.py
@@ -4,4 +4,15 @@ setup(
     name="nonebot-plugin-learning-chat",
     version="0.4.1",
     packages=find_packages(),
+    install_requires=[
+        "nonebot2[fastapi]>=2.0.0,<3.0.0",
+        "nonebot-adapter-onebot>=2.1,<3.0.0",
+        "nonebot-plugin-apscheduler>=0.5.0,<1.0.0",
+        "tortoise-orm>=0.20.0",
+        "jieba>=0.42.1,<1.0.0",
+        "ruamel.yaml>=0.17.21,<1.0.0",
+        "amis-python @ git+https://github.com/trytodupe/amis-python.git@85442e9bc49243043b942926be5c834d04cf4295",
+        "python-jose>=3.3.0,<4.0.0",
+        "nonebot-plugin-tortoise-orm>=0.1.1,<1.0.0",
+    ],
 )
